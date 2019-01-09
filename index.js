f3858b6f5bfaef37a2c4d96f771695f4cbed5cc2
rwutscher <ricwu01@gmail.com>
2019-01-09 11:14:37 +0100

    commit

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
+app.get('/', (req,res)=>{
+    
+})
+
 http.listen(3000, (err) => {
     console.log('Server running')
 })
