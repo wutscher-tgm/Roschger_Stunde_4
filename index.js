24c1f9e93541502d1c75a6d89b773cbb73168ed0
rwutscher <ricwu01@gmail.com>
2019-01-09 11:29:17 +0100

    commit

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ app.get('/', (req,res)=>{
     res.sendFile(__dirname + '/index.html')
 })
 
+io.on('connection', (socket)=>{
+    console.log('Neuer Benutzer')
+    socket.on('message', (msg)=>{
+        console.log(msg)
+    })
+})
+
 http.listen(3000, (err) => {
     console.log('Server running')
 })
