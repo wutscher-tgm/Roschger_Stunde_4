0b1882a7e0255ac64139bc64f1fb897fd897f552
rwutscher <ricwu01@gmail.com>
2019-01-09 11:15:52 +0100

    commit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http').Server(app)
 const io = require('socket.io')(http)
 
 app.get('/', (req,res)=>{
-    
+    res.sendFile(__dirname + 'index.html')
 })
 
 http.listen(3000, (err) => {
